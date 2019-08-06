fae84ef711bf99fe94e16f92977efa3d4fab9225
Ushana Bandyopadhyay <dj.indiana35@gmail.com>
2019-08-07 01:43:41 +0530

    Add files via upload

---
+++ FINAL.py
@@ -0,0 +1,67 @@
+import subprocess
+
+input1 = ['4','2','1']
+input2 = ['2','2','1','1','2']
+input3 = ['1','1','3','1','y','1','1','1','7'] 
+
+
+	for x in input1:
+		
+		if ( x=='1'):
+			first = subprocess.Popen(['/bin/echo', x], stdout=subprocess.PIPE)
+	        	second = subprocess.Popen(['bash', './fluxion.sh'], stdin=first.stdout)
+  			#Close the FluxionScanner Terminal after 10s
+			script output.txt
+			break
+
+		first = subprocess.Popen(['/bin/echo', x], stdout=subprocess.PIPE)
+		second = subprocess.Popen(['bash', './fluxion.sh'], stdin=first.stdout)
+    	
+		
+ 
+
+
+file=open("output.txt","r")
+
+for line in file:
+	print line
+
+t=raw_input('Make a Choice:')
+
+input2.insert(0,t)
+
+#HandshakeSnooper
+
+
+i=0	
+
+	for x in input2:
+
+		if (i==(len(input2)-1)):
+			first = subprocess.Popen(['/bin/echo', x], stdout=subprocess.PIPE)
+			second = subprocess.Popen(['bash', './fluxion.sh'], stdin=first.stdout)
+				
+			if(): #Checking whether a new folder has been added or not
+				break
+		
+		first = subprocess.Popen(['/bin/echo', x], stdout=subprocess.PIPE)
+		second = subprocess.Popen(['bash', './fluxion.sh'], stdin=first.stdout)
+	
+		i=i+1
+    		
+		
+    
+
+#CaptivePortal
+
+	for x in input3:
+		
+		first = subprocess.Popen(['/bin/echo', x], stdout=subprocess.PIPE)
+		second = subprocess.Popen(['bash', './fluxion.sh'], stdin=first.stdout)
+    
+ 
+
+
+
+
+
